6220fe801fc068edc2a3ce5d21ac864e03749949
seenkoo <shinkupro@gmail.com>
2021-12-01 17:07:41 +0500

    fix slug route constraint not working with cyrillic symbols

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   get '/', to: 'pages#index', as: :pages
   get '(/*slug)/add', to: 'pages#new', as: :new_page
   get '/*slug/edit', to: 'pages#edit', as: :edit_page
-  get '/*slug', to: 'pages#show', as: :page
+  get '/*slug', to: 'pages#show', as: :page, constraints: ->(request) {
+    slug = request.path_parameters[:slug]
+    slug.match? /\A[a-zA-Zа-яА-ЯёЁ0-9_\/]+\z/
+  }
   patch '/*slug', to: 'pages#update'
   put '/*slug', to: 'pages#update'
   delete '/*slug', to: 'pages#destroy'
