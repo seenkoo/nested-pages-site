b499e7f31ba21f2ab9f8b38fc33eae351beeb238
seenkoo <shinkupro@gmail.com>
2021-12-01 17:12:11 +0500

    add PagesController#show

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,17 @@
 class PagesController < ApplicationController
+  include ErrorHandling
+
+  before_action :set_page!, only: %i[show]
+
   def index
     @pages = Page.all.arrange
+  end
+
+  def show; end
+
+  private
+
+  def set_page!
+    @page = Page.find_by!(slug: params[:slug])
   end
 end
