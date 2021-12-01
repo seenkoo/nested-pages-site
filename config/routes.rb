ee26cee197535218f1f2e90a2825bcb41c807ab7
seenkoo <shinkupro@gmail.com>
2021-12-01 15:54:41 +0500

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/', to: 'pages#index', as: :pages
+  get '(/*slug)/add', to: 'pages#new', as: :new_page
+  get '/*slug/edit', to: 'pages#edit', as: :edit_page
+  get '/*slug', to: 'pages#show', as: :page
+  patch '/*slug', to: 'pages#update'
+  put '/*slug', to: 'pages#update'
+  delete '/*slug', to: 'pages#destroy'
+  post '/*slug', to: 'pages#create'
+
+  root 'pages#index'
 end
